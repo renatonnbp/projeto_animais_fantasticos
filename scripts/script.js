5d58c2c173681b31f73c34146d84a363c6b504ed
Renato Nunes <renatonunesan@gmail.com>
2024-03-10 21:06:05 -0300

    Update animation scroll effect

--- scripts/script.js
+++ scripts/script.js
@@ -40,3 +40,47 @@ function initAccordion() {
     }
 }
 initAccordion();
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
+    });
+}
+initScrollSuave();
+
+function animacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if(sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if(isSectionVisible) {
+                    section.classList.add(activeClass);
+                } else {
+                    section.classList.remove(activeClass);
+                }
+            });
+        }
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+animacaoScroll();
