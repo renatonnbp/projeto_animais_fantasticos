964252eb144127e59cdcd06a4ef014dec8714e20
Renato Nunes <renatonnbp@gmail.com>
2024-03-13 19:14:05 -0300

    new tooltip.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initScrollSuave from './modules/scroll-suave.js'
 import initAccordion from './modules/accordion.js'
 import animacaoScroll from './modules/anima-scroll.js'
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initTabNav();
 initScrollSuave();
 initAccordion();
 animacaoScroll();
 initModal();
+initTooltip();
