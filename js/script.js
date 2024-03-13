97a8b840ef6c5067ed0c5e3abc88d8100b868979
Renato Nunes <renatonunesan@gmail.com>
2024-03-13 20:13:28 -0300

    new dropdown-menu.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-import initTabNav from './modules/tabnav.js'
-import initScrollSuave from './modules/scroll-suave.js'
-import initAccordion from './modules/accordion.js'
-import animacaoScroll from './modules/anima-scroll.js'
-import initModal from './modules/modal.js'
-import initTooltip from './modules/tooltip.js'
+import initTabNav from './modules/tabnav.js';
+import initScrollSuave from './modules/scroll-suave.js';
+import initAccordion from './modules/accordion.js';
+import animacaoScroll from './modules/anima-scroll.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initTabNav();
 initScrollSuave();
@@ -11,3 +12,4 @@ initAccordion();
 animacaoScroll();
 initModal();
 initTooltip();
+initDropdownMenu();
