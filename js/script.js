1b0976b3593c5818ce173678509d58d51b863df4
Renato Nunes <renatonnbp@gmail.com>
2024-03-13 13:05:50 -0300

    Update modules

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initTabNav from './modules/tabnav.js'
+import initScrollSuave from './modules/scroll-suave.js'
+import initAccordion from './modules/accordion.js'
+import animacaoScroll from './modules/anima-scroll.js'
+
+initTabNav();
+initScrollSuave();
+initAccordion();
+animacaoScroll();
