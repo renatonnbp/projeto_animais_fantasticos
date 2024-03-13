c6ed5ae2545be1a4d69a2116a5a283f4900a7593
Renato Nunes <renatonnbp@gmail.com>
2024-03-13 14:17:40 -0300

    Update modal.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initTabNav from './modules/tabnav.js'
 import initScrollSuave from './modules/scroll-suave.js'
 import initAccordion from './modules/accordion.js'
 import animacaoScroll from './modules/anima-scroll.js'
+import initModal from './modules/modal.js'
 
 initTabNav();
 initScrollSuave();
 initAccordion();
 animacaoScroll();
+initModal();
