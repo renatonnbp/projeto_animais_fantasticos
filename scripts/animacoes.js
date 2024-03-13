20bd7efcaa2d00d418cdf2a2cbbed8851a72995c
Renato Nunes <renatonnbp@gmail.com>
2024-03-13 11:24:55 -0300

    Update scripts folder

---
+++ scripts/animacoes.js
@@ -0,0 +1,86 @@
+const activeClass = 'ativo';
+
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add(activeClass);
+
+        function activeTab(index) {
+            tabContent.forEach((item) => {
+                item.classList.remove(activeClass);
+            });
+            tabContent[index].classList.add(activeClass);
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
+        });
+    }
+}
+initTabNav();
+
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    if(accordionList.length) {
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+initAccordion();
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
+    });
+}
+initScrollSuave();
+
+function animacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if(sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if(isSectionVisible) {
+                    section.classList.add(activeClass);
+                } else {
+                    section.classList.remove(activeClass);
+                }
+            });
+        }
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+animacaoScroll();
