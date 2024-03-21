ed1f884ba7b79f9c1f8f27f80b8d57d0181d63ba
Renato Nunes <renatonnbp@gmail.com>
2024-03-21 14:49:50 -0300

    new fetch-bitcoin

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,12 @@
+export default function initFetchBitcoin() {
+    fetch('https://blockchain.info/ticker')
+    .then(response => response.json())
+    .then(json => {
+        const btcPreco = document.querySelector('.btc-preco');
+        btcPreco.innerText = (1000 / json.BRL.buy).toFixed(4);
+    }) .catch(erro => {
+        console.log(Error(erro));
+    })
+}
+
+
--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menumobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 initTabNav();
 initScrollSuave();
@@ -19,3 +20,4 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
+initFetchBitcoin();
