97f7314a71a8d223719b5cd6cd3cd8f3d6b04052
Renato Nunes <renatonunesan@gmail.com>
2024-03-26 13:57:55 -0300

    Automation modules with ESLint

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -1,24 +1,22 @@
 export default function animacaoScroll() {
-    const sections = document.querySelectorAll('[data-anime="scroll"]');
-    const activeClass = 'ativo';
-    
-    if(sections.length) {
-        const windowMetade = window.innerHeight * 0.6;
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  const activeClass = 'ativo';
+  const windowMetade = window.innerHeight * 0.6;
 
-        function animaScroll() {
-            sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top;
-                const isSectionVisible = (sectionTop - windowMetade) < 0;
-                if(isSectionVisible) {
-                    section.classList.add(activeClass);
-                } else if(section.classList.contains(activeClass)) {
-                    section.classList.remove(activeClass);
-                }
-            });
-        }
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      if (isSectionVisible) {
+        section.classList.add(activeClass);
+      } else if (section.classList.contains(activeClass)) {
+        section.classList.remove(activeClass);
+      }
+    });
+  }
 
-        animaScroll();
-
-        window.addEventListener('scroll', animaScroll);
-    }
+  if (sections.length) {
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
 }
--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,29 +1,28 @@
 import initAnimaNumeros from './anima-numeros.js';
 
 export default function initFetchAnimais() {
-    async function fetchAnimais(url) {
-        try {
-            const animaisResponse = await fetch(url);
-            const animaisJSON = await animaisResponse.json();
-            const numerosGrid = document.querySelector('.numeros-grid');
+  function createAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
+    return div;
+  }
 
-            animaisJSON.forEach(animal => {
-                const divAnimal = createAnimal(animal);
-                numerosGrid.appendChild(divAnimal);
-            });
-            initAnimaNumeros();
-        } catch(erro) {
-            console.log(Error(erro))
-        }
+  async function fetchAnimais(url) {
+    try {
+      const animaisResponse = await fetch(url);
+      const animaisJSON = await animaisResponse.json();
+      const numerosGrid = document.querySelector('.numeros-grid');
+
+      animaisJSON.forEach((animal) => {
+        const divAnimal = createAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+      });
+      initAnimaNumeros();
+    } catch (erro) {
+      console.log(Error(erro));
     }
+  }
 
-    function createAnimal(animal) {
-        const div = document.createElement('div');
-        div.classList.add('numero-animal');
-        div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
-        return div;
-    }
-
-    fetchAnimais('./animaisapi.json');
+  fetchAnimais('./animaisapi.json');
 }
-
