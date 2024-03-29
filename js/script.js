f335dae4e4db245c14a3d3b6dd71a69bdabe1aa1
Renato Nunes <renatonunesan@gmail.com>
2024-03-29 14:21:03 -0300

    menu mobile refatorado

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import Accordion from './modules/accordion.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menumobile.js';
+import MenuMobile from './modules/menumobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
@@ -31,7 +31,9 @@ scrollAnima.init();
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
 
-initMenuMobile();
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]', 'click', 'touchstart');
+menuMobile.init();
+
 initFuncionamento();
 
 fetchAnimais('../../animaisapli.json', 'numeros-grid');
